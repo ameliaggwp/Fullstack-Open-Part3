73db4c2d419ffa55f1fd26cadc0a46cec3b097fc
unknown <ameliaggwp@gmail.com>
2020-06-16 13:05:12 -0700

    3.1: Phonebook backend step1

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    name: "Harry Potter",
+    number: "132-456-7890",
+    id: 1,
+  },
+  {
+    name: "Hermione Granger",
+    number: "236-456-7890",
+    id: 2,
+  },
+  {
+    name: "Ron Weasley",
+    number: "741-547-2518",
+    id: 3,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Phonebook Backend</h1>");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
