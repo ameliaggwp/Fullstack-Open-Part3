fd78868368f504eb9a6cb7dc28ee8318c1d69760
unknown <ameliaggwp@gmail.com>
2020-07-01 10:48:44 -0700

    Adding validators to PUT request and specifying that only the number be updated

--- index.js
+++ index.js
@@ -74,12 +74,11 @@ app.post("/api/persons", (req, res, next) => {
 app.put("/api/persons/:id", (req, res, next) => {
   const body = req.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-
-  Person.findByIdAndUpdate({ _id: `${req.params.id}` }, person, { new: true })
+  Person.findByIdAndUpdate(
+    req.params.id,
+    { number: body.number },
+    { new: true, runValidators: true }
+  )
     .then((updatedPerson) => {
       res.json(updatedPerson);
     })
